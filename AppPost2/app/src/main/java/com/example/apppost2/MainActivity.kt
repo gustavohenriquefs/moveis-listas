1bed24da32e919244362f06419cffe1d12986a71
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2025-01-20 23:52:40 -0300

    feat: add AppPost2

---
+++ AppPost2/app/src/main/java/com/example/apppost2/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.apppost2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import com.example.apppost2.ui.screens.PostScreen
+import com.example.apppost2.ui.screens.UserScreen
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+
+import android.annotation.SuppressLint
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    var selectTab by remember { mutableStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { "PostApp" },
+                backgroundColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            )
+        },
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    selected = selectTab == 0,
+                    onClick = { selectTab = 0 },
+                    label = { Text(text = "Usuários") },
+                    icon = { Icon(Icons.Default.Person, contentDescription = "Usuarios") }
+                )
+
+                BottomNavigationItem(
+                    selected = selectTab == 1,
+                    onClick = { selectTab = 1 },
+                    label = { Text(text = "Posts") },
+                    icon = { Icon(Icons.Default.List, contentDescription = "Posts") }
+                )
+            }
+        }
+    ) {
+        when (selectTab) {
+            0 -> UserScreen()
+            1 -> PostScreen()
+        }
+    }
+}
